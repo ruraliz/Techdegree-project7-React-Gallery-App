9231b55ac4874aad9a9a48d683d409d7f7ee2236
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-10-11 13:17:23 -0700

    updated app.js file

--- react-gallery-app/src/App.js
+++ react-gallery-app/src/App.js
@@ -15,82 +15,46 @@ function App() {
   const [dogs, setDogs]= useState([]);
   const [cats, setCats]= useState([]);
   const [computers, setComputers]= useState([]);
-  
-  const fetchData = (query) => {
+  const fetchData = (query) =>{
     setLoading(true);
     let activeFetch = true;
-    axios.get(
-      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
-    ).then(response => {
-      if (activeFetch) {
-        if (query === "dogs") {
-          setDogs(response.data.photos.photo);
-        } else if (query === "cats") {
-          setCats(response.data.photos.photo);
-        } else if (query === "computers") {
-          setComputers(response.data.photos.photo);
-        } else {
-          setImages(response.data.photos.photo)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        if (activeFetch) {
+          if(query === "dogs"){
+            setDogs(response.data.photos.photo);
+          }else if(query === "cats"){
+            setCats(response.data.photos.photo);
+          }else if(query === "computers"){
+            setComputers(response.data.photos.photo);
+          }else{
+            setImages(response.data.photos.photo);
+          }
+          setLoading(false);
         }
-        setLoading(false);
-      }
-    })
+      })
       .catch(error => {
-        console.log("This is the error: ", error);
+        console.log("Error fetching and parsing data", error);
       });
-
-    return () => { activeFetch = false; }
+    return () => { activeFetch = false }
+  }
+  const handleQueryChange = (searchText) => {
+    setQuery(searchText);
   };
-
-  const changeQuery = (newQuery) => {
-    setQuery(newQuery);
-  };
-
-
   useEffect(() => {
     fetchData(query);
   }, [query]);
 
-  // const fetchData = (query) =>{
-  //   setLoading(true);
-  //   let activeFetch = true;
-  //   axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-  //     .then(response => {
-  //       if (activeFetch) {
-  //         if(query === "dogs"){
-  //           setDogs(response.data.photos.photo);
-  //         }else if(query === "cats"){
-  //           setCats(response.data.photos.photo);
-  //         }else if(query === "computers"){
-  //           setComputers(response.data.photos.photo);
-  //         }else{
-  //           setImages(response.data.photos.photo);
-  //         }
-  //         setLoading(false);
-  //       }
-  //     })
-  //     .catch(error => {
-  //       console.log("Error fetching and parsing data", error);
-  //     });
-  //   return () => { activeFetch = false }
-  // }
-  // const handleQueryChange = (searchText) => {
-  //   setQuery(searchText);
-  // };
-  // useEffect(() => {
-  //   fetchData(query);
-  // }, [query]);
-
   return (
     <div className="App">
-      <SearchForm changeQuery={changeQuery} />
-      <Nav changeQuery={changeQuery}  />
+      <SearchForm changeQuery={handleQueryChange} />
+      <Nav changeQuery={handleQueryChange}  />
       <Routes>
         <Route path="/" element={<Navigate replace to="/dogs" />} />
-        <Route path="/dogs" element={<PhotoContainer data={dogs} loading={loading} query={"dogs"} changeQuery={changeQuery}/>} />
-        <Route path="/cats" element={<PhotoContainer data={cats} loading={loading} query={"cats"} changeQuery={changeQuery} />} />
-        <Route path="/computers" element={<PhotoContainer data={computers} loading={loading} query={"computers"} changeQuery={changeQuery} />} />
-        <Route path="/search/:topic" element={<PhotoContainer loading={loading} data={images} query={query} changeQuery={changeQuery} />} />
+        <Route path="/dogs" element={<PhotoContainer data={dogs} loading={loading} query={"dogs"} changeQuery={handleQueryChange} />} />
+        <Route path="/cats" element={<PhotoContainer data={cats} loading={loading} query={"cats"} changeQuery={handleQueryChange} />} />
+        <Route path="/computers" element={<PhotoContainer data={computers} loading={loading} query={"computers"} changeQuery={handleQueryChange} />} />
+        <Route path="/search/:topic" element={<PhotoContainer loading={loading} data={images} query={query} changeQuery={handleQueryChange} />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
